9848d6a21a7c141d2d651c1a0ced601aae9b4b51
mixsoda <39870720+mixsoda@users.noreply.github.com>
2018-06-03 21:07:42 +0900

    added ctrl_aircon.sh

---
+++ ctrl/ctrl_aircon.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# useage ::
+# ctrl_aircon (on|off) mode [temp]
+# mode (c|w)
+# temp temperature
+
+TOKEN=`cat ../token.txt`
+APPLIANCE_ID=`cat ../aircon_id.txt`
+
+MODE_NAME=""
+TEMPSTR=""
+
+case $1 in
+    "off"  ) POWER="button=power-off" ;;
+    "on"  ) POWER="button=" ;;
+    * )    exit ;;
+esac  
+
+if [ $# -eq 3 ]; then 
+    case $2 in
+        "cool" | "c" ) MODE_NAME="operation_mode=cool&" ;;
+        "warm" | "w"  ) MODE_NAME="operation_mode=warm&" ;;
+        * )    exit ;;
+    esac
+fi
+
+
+if [ $# -eq 3 ]; then 
+    TEMPSTR="temperature=${3}&"
+fi
+
+echo "command :: ${MODE_NAME}${TEMPSTR}${POWER}"
+
+#get remo sensor datadevices
+result=`curl -s -X POST "https://api.nature.global/1/appliances/${APPLIANCE_ID}/aircon_settings" -d "${MODE_NAME}${TEMPSTR}${POWER}" -k -H "Authorization: Bearer ${TOKEN}"`
+
+echo ${result}
