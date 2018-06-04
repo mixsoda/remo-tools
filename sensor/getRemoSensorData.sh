f466d4d7582912d19b78ac0305f9b86dee8fbd31
mixsoda <39870720+mixsoda@users.noreply.github.com>
2018-06-05 01:33:35 +0900

    remove sensor/parsrj.sh

--- sensor/getRemoSensorData.sh
+++ sensor/getRemoSensorData.sh
@@ -9,21 +9,21 @@ NOW=`date +"%Y-%m-%dT%T"`
 curl -s -X GET "https://api.nature.global/1/devices" -H "accept: application/json" --header "Authorization: Bearer ${TOKEN}" > logs/out_rawdata.json
 
 #get number of sensor data
-sensor_num=`./parsrj.sh logs/out_rawdata.json | grep "newest_events" | wc -l`
+sensor_num=`../lib/parsrj.sh logs/out_rawdata.json | grep "newest_events" | wc -l`
 sensor_num=`expr ${sensor_num} / 2`
 
 #get firmware_version
-firm_var=`./parsrj.sh logs/out_rawdata.json | grep "firmware_version" | cut -d" " -f2`
-firm_date=`./parsrj.sh logs/out_rawdata.json | grep "0].updated_at" | cut -d" " -f2` 
+firm_var=`../lib/parsrj.sh logs/out_rawdata.json | grep "firmware_version" | cut -d" " -f2`
+firm_date=`../lib/parsrj.sh logs/out_rawdata.json | grep "0].updated_at" | cut -d" " -f2` 
 
 #perse sensor data using parsrj.sh
 #https://github.com/ShellShoccar-jpn/Parsrs
-hu_value=`./parsrj.sh logs/out_rawdata.json | grep "newest_events.hu.val" | cut -d" " -f2`
-hu_updated=`./parsrj.sh logs/out_rawdata.json | grep "newest_events.hu.created_at" | cut -d" " -f2`
-il_value=`./parsrj.sh logs/out_rawdata.json | grep "newest_events.il.val" | cut -d" " -f2`
-il_updated=`./parsrj.sh logs/out_rawdata.json | grep "newest_events.il.created_at" | cut -d" " -f2`
-temp_value=`./parsrj.sh logs/out_rawdata.json | grep "newest_events.te.val" | cut -d" " -f2`
-temp_updated=`./parsrj.sh logs/out_rawdata.json | grep "newest_events.te.created_at" | cut -d" " -f2`
+hu_value=`../lib/parsrj.sh logs/out_rawdata.json | grep "newest_events.hu.val" | cut -d" " -f2`
+hu_updated=`../lib/parsrj.sh logs/out_rawdata.json | grep "newest_events.hu.created_at" | cut -d" " -f2`
+il_value=`../lib/parsrj.sh logs/out_rawdata.json | grep "newest_events.il.val" | cut -d" " -f2`
+il_updated=`../lib/parsrj.sh logs/out_rawdata.json | grep "newest_events.il.created_at" | cut -d" " -f2`
+temp_value=`../lib/parsrj.sh logs/out_rawdata.json | grep "newest_events.te.val" | cut -d" " -f2`
+temp_updated=`../lib/parsrj.sh logs/out_rawdata.json | grep "newest_events.te.created_at" | cut -d" " -f2`
 
 #echo "humidity = $hu_value  (updated: $hu_updated )"
 #echo "tempreture = $temp_value (updated: $temp_updated )"
