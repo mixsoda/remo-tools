58e8c7cd1c5328712234317ac6d497527f598125
mixsoda <39870720+mixsoda@users.noreply.github.com>
2018-06-26 01:11:02 +0900

    Added get_aircon_settings.sh

---
+++ utils/get_aircon_settings.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+
+CDIR=`dirname ${0}`
+cd ${CDIR}/
+
+TOKEN=`cat ../token.txt`
+TARGET_AIRCON_ID=`cat ../aircon_id.txt`
+
+#get remo sensor datadevices
+curl -s -X GET "https://api.nature.global/1/appliances" -H "accept: application/json" --header "Authorization: Bearer ${TOKEN}" > result_models.json
+
+#get aircon index
+aircon_index=`../lib/parsrj.sh result_models.json  | grep ${TARGET_AIRCON_ID} | awk 'match($0, /\$\[[0-9]\]/) { print substr($0,RSTART,RLENGTH) }'`
+
+#output setting
+key="${aircon_index}.settings.${1}"
+
+echo `../lib/parsrj.sh result_models.json  | grep -F ${key} | cut -d" " -f 2`
